75e8043138e09ad9c06f2d32340e81601bcb79c8
Firax_Cobalt <123969490+Firax-Cobalt@users.noreply.github.com>
2024-07-15 09:37:58 +0500

    Стилизовал комментарий (ревью)  после формы.

--- index.php
+++ index.php
@@ -100,38 +100,36 @@
       </form>
     </section>
     <section class="reviews wrapper">
-      <div class="wrapper">
-        <div class="reviews__wrapper">
-          <div class="review">
-            <div class="review__image-wrapper">
-              <img class="review__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/reviews/review-1.jpg" alt="Иван Грачев" width="50" height="50" />
-            </div>
-            <p class="review__name">Иван Грачев</p>
-            <p class="review__text">Очень самобытный и необычный проект.</p>
+      <ul class="reviews__list">
+        <li class="reviews__item">
+          <div class="review__image-wrapper">
+            <img class="review__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/reviews/review-1.jpg" alt="Иван Грачев" width="50" height="50" />
           </div>
-          <div class="review">
-            <div class="review__image-wrapper">
-              <img class="review__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/reviews/review-2.jpg" alt="Мария Рачинская" width="50" height="50" />
-            </div>
-            <p class="review__name">Мария Рачинская</p>
-            <p class="review__text">
-              Мне особенно понравилось решение лестничных клеток. Кружки
-              вообще очень стильные!
-            </p>
+          <p class="review__name">Иван Грачев</p>
+          <p class="review__text">Очень самобытный и необычный проект.</p>
+        </li>
+        <li class="reviews__item">
+          <div class="review__image-wrapper">
+            <img class="review__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/reviews/review-2.jpg" alt="Мария Рачинская" width="50" height="50" />
           </div>
-          <div class="review">
-            <div class="review__image-wrapper">
-              <img class="review__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/reviews/review-3.jpg" alt="Светлана" width="50" height="50" />
-            </div>
-            <p class="review__name">Светлана</p>
-            <p class="review__text">
-              Очень нравится, что в городе развиваются такие проекты. Надеюсь,
-              такие проекты скоро будут не только в частных домах, но и в
-              кафе, музеях, театрах. Я бы сходила!
-            </p>
+          <p class="review__name">Мария Рачинская</p>
+          <p class="review__text">
+            Мне особенно понравилось решение лестничных клеток. Кружки
+            вообще очень стильные!
+          </p>
+        </li>
+        <li class="reviews__item">
+          <div class="review__image-wrapper">
+            <img class="review__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/reviews/review-3.jpg" alt="Светлана" width="50" height="50" />
           </div>
-        </div>
-      </div>
+          <p class="review__name">Светлана</p>
+          <p class="review__text">
+            Очень нравится, что в городе развиваются такие проекты. Надеюсь,
+            такие проекты скоро будут не только в частных домах, но и в
+            кафе, музеях, театрах. Я бы сходила!
+          </p>
+        </li>
+      </ul>
     </section>
   </footer>
 </body>
